64a72c7924e56c8becde1a5b07043ba9a05888c2
Huzaifa-1100 <asadhussainshad@gmail.com>
2024-09-15 02:17:24 +0500

    add dynamic post data

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,20 +1,26 @@
-"use client"
+"use client";
 import { PostCard } from "@/components/PostCard/PostCard";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Blog() {
+  const [PostData, setPostData] = useState([]);
+
   useEffect(() => {
     getAllPost();
   }, []);
   const getAllPost = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await response.json()
+    const data = await response.json();
+    setPostData(data);
     console.log(data);
-    
   };
+  console.log(PostData);
+
   return (
-    <div>
-      <PostCard />
+    <div className="flex flex-wrap items-center justify-center">
+      {PostData.map((item) => {
+        return <PostCard postData={item} />
+      })}
     </div>
   );
 }
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,32 +1,29 @@
-import Image from "next/image"
+import Image from "next/image";
 
- export const PostCard = () => {
-   return (
-     <div>
-         <div className="flex">
-                <div className="max-w-sm rounded shadow-lg bg-white m-4">
-                    {/* Post Image */}
-                    <Image
-                        className="w-full"
-                        src="https://via.placeholder.com/400x200"
-                        alt="Post Image"
-                        width={500}
-                        height={500}
-                    />
+export const PostCard = (props: any) => {
+  const { postData } = props;
+  console.log(`postData ${postData}`);
 
-                    {/* Post Content */}
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">Post Title</div>
-                        <p className="text-gray-700 text-base">
-                            sadfjaskldfjljsdf;kj
-                        </p>
-                    </div>
+  return (
+    <div>
+      <div className="flex">
+        <div className="max-w-sm rounded shadow-lg bg-white m-4">
+          {/* Post Image */}
+          <Image
+            className="w-full"
+            src="https://via.placeholder.com/400x200"
+            alt="Post Image"
+            width={500}
+            height={500}
+          />
 
-
-                </div>
-
-            </div>
-     </div>
-   )
- }
- 
+          {/* Post Content */}
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2 text-black"> {postData.title} </div>
+            <p className="text-gray-700 text-base">{postData.body}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
