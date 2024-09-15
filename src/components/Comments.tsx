dc8bcb7f0a4ce0e62a9b7b7f255cc70eb12f2c17
Huzaifa-1100 <asadhussainshad@gmail.com>
2024-09-15 20:24:25 +0500

    addin avatars to the post and comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,28 +1,36 @@
 import React from "react";
+import Image from "next/image";
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
 
-export default function Comments({commentData}:any) {
+export default function Comments({ commentData }: any) {
   return (
     <div>
       <div className=" bg-gray-100 p-8">
-        <div className="max-full mx-auto bg-white shadow-md rounded-lg my-4 p-6">
+        <div className="max-full mx-auto bg-white shadow-gray-900 shadow-md hover:shadow-lg hover:shadow-gray-900 rounded-lg my-4 p-6">
           {/* Commenter Information */}
           <div className="flex items-center mb-4">
-            <div className="mr-4">
-              <img
-                className="w-10 h-10 rounded-full"
-                src={`https://ui-avatars.com/api/?name=${commentData.name}&background=random`}
-                alt={commentData.name}
-              />
+            <div className=" h-12 w-12 gap-4 items-center">
+              <Avatar className="rounded-full" src={`https://i.pravatar.cc/150?u=${commentData.name}`} />
+              <Avatar className="text-xl font-bold" name={commentData.name} />
+              
             </div>
             <div>
-              <p className="text-lg font-semibold">{commentData.email}</p>
+              <p className="text-lg font-semibold mt-">{commentData.email}</p>
             </div>
           </div>
 
           {/* Comment Body */}
-          <p className="text-gray-700 text-base">{commentData.body}</p>
+          <p className="text-gray-700 text-base ">{commentData.body}</p>
         </div>
       </div>
     </div>
   );
 }
+
+// <Image
+//                 className="w-10 h-10 rounded-full"
+//                 src={`https://ui-avatars.com/api/?name=${commentData.name}&background=random`}
+//                 alt={commentData.name}
+//                 width={100}
+//                 height={100}
+//               />
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
 export const PostCard = (props: any) => {
   const { postData } = props;
   console.log(`postData ${postData}`);
@@ -9,13 +9,10 @@ export const PostCard = (props: any) => {
       <div className="flex">
         <div className="max-w-sm rounded shadow-lg bg-white m-4">
           {/* Post Image */}
-          <Image
-            className="w-full"
-            src="https://via.placeholder.com/400x200"
-            alt="Post Image"
-            width={500}
-            height={500}
-          />
+          <div className="align-top">
+            <Avatar src={`https://i.pravatar.cc/150?u=${postData.id}`} size="sm" />
+            {/* <Avatar className="text-xl font-bold" name={postData.name} /> */}
+          </div>
 
           {/* Post Content */}
           <div className="px-6 py-4">
@@ -31,3 +28,11 @@ export const PostCard = (props: any) => {
     </div>
   );
 };
+
+// <Image
+//   className="w-full"
+//   src="https://via.placeholder.com/400x200"
+//   alt="Post Image"
+//   width={500}
+//   height={500}
+// />;
