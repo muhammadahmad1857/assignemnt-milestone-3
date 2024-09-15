c9f4aa394c4d4fecc8caf043922eae62fd492b3f
Huzaifa-1100 <asadhussainshad@gmail.com>
2024-09-15 21:48:59 +0500

    type error resolve

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -2,7 +2,19 @@ import React from "react";
 
 import { Avatar } from "@nextui-org/avatar";
 
-export default function Comments({ commentData }: any) {
+interface CommentDataType{
+  body: string;
+  email: string;
+  id: number;
+  name: string;
+  postId: number;
+ 
+
+}
+
+export default function Comments({commentData}:{commentData:CommentDataType}) {
+  console.log(commentData);
+
   return (
     <div>
       <div className=" bg-gray-100 p-8">
@@ -10,9 +22,11 @@ export default function Comments({ commentData }: any) {
           {/* Commenter Information */}
           <div className="flex items-center mb-4">
             <div className=" h-12 w-12 gap-4 items-center">
-              <Avatar className="rounded-full" src={`https://i.pravatar.cc/150?u=${commentData.name}`} />
+              <Avatar
+                className="rounded-full"
+                src={`https://i.pravatar.cc/150?u=${commentData.name}`}
+              />
               <Avatar className="text-xl font-bold" name={commentData.name} />
-              
             </div>
             <div>
               <p className="text-lg font-semibold mt-">{commentData.email}</p>
